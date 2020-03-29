ecd0fb17b9d8647315867f0866287584926f0d78
Joe Casini <joe.casini.spam@gmail.com>
2020-03-29 16:19:13 +0200

    Google Analytics event tracking per i click sui pulsanti di condivisione

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
 function googleTranslateElementInit() {
   new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
 }
+
+(function($){
+
+	var sendAnalyticsEvent = function (eventLabel) {
+		ga('send', 'event', 'Share', 'Share', eventLabel, 1);
+	}
+
+	$(document).on('ready', function(){
+
+		$('.share a.btn').on('click', function(){
+			var href = $(this).attr('href'),
+				eventLabel = ""
+			
+			if( href.indexOf('twitter.com') >= 0 )
+				eventLabel = 'Twitter';
+			else if( href.indexOf('wa.me') >= 0 )
+				eventLabel = 'Whatsapp';
+			else if( href.indexOf('facebook.com') >= 0 )
+				eventLabel = 'Facebook';
+
+			sendAnalyticsEvent(eventLabel)
+		})
+
+	})
+        
+})(jQuery);
