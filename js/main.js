d45b001dc99502ba640736c9b3ef7f01b9428752
Joe Casini <joe.casini.spam@gmail.com>
2020-03-29 16:25:15 +0200

    FIX Google Analytics event JS

--- js/main.js
+++ js/main.js
@@ -10,24 +10,20 @@ function googleTranslateElementInit() {
 		ga('send', 'event', 'Share', 'Share', eventLabel, 1);
 	}
 
-	$(document).on('ready', function(){
+	$('.share a.btn').on('click', function(){
+		var href = $(this).attr('href'),
+			eventLabel = ""
+		
+		if( href.indexOf('twitter.com') >= 0 )
+			eventLabel = 'Twitter';
+		else if( href.indexOf('wa.me') >= 0 )
+			eventLabel = 'Whatsapp';
+		else if( href.indexOf('facebook.com') >= 0 )
+			eventLabel = 'Facebook';
 
-		$('.share a.btn').on('click', function(){
-			var href = $(this).attr('href'),
-				eventLabel = ""
-			
-			if( href.indexOf('twitter.com') >= 0 )
-				eventLabel = 'Twitter';
-			else if( href.indexOf('wa.me') >= 0 )
-				eventLabel = 'Whatsapp';
-			else if( href.indexOf('facebook.com') >= 0 )
-				eventLabel = 'Facebook';
+		console.debug('Share on '+eventLabel)
 
-			console.debug('Share on '+eventLabel)
-
-			sendAnalyticsEvent(eventLabel)
-		})
-
+		sendAnalyticsEvent(eventLabel)
 	})
         
 })(jQuery);
